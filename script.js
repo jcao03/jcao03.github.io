2f33178dfdc038493734e3da0865cd7af512c6e2
jcao03 <92761025+jcao03@users.noreply.github.com>
2021-10-20 12:00:51 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-a = [];
 function addName() {
-    var b = document.getElementById("name()").value
-    a.push(b)
-    window.alert(a)
+    var b = document.getElementById("name").value;
+    document.getElementById("name").value = "";
+    window.alert(b)
 }
