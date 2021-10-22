058d397b67e6b09678742e225fa954514b7aa754
jcao03 <92761025+jcao03@users.noreply.github.com>
2021-10-22 10:57:59 -1000

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,17 @@ function addName() {
     var b = document.getElementById("name").value;
     a.push(b);
     document.getElementById("name").value = "";
-    window.alert(a)
+    for (var i = 0; i < a.length; i++) {
+  //create variable in the form of ordered list
+    var newP = document.createElement("ol");
+  //create varaible that is like "the home" for the element
+    var newText = document.createTextNode(a[i]);
+  //bind the element to its parent
+    newP.appendChild(newText);
+  //bind the appended pair to the body
+    document.body.appendChild(newP);
+  }
+  //clear array
+  a = []
+  window.alert(a)
 }
