91dda42b93fa4ec42d34cd200ae459abe3662043
jcao03 <92761025+jcao03@users.noreply.github.com>
2021-10-22 11:18:05 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+
+var a;
 function addName() {
+//get value from input field and save as "a"
     var b = document.getElementById("name").value;
-    documment.getElementById("name").value = "";
     window.alert(b)
+//create localStorage key with value of a
+    localStorage.setItem("uName", a);
+//automatically take user to page two.html
+    location.href = "two.html";
 }
