8fc9451e86ca5af33f47608da68cafe20ee9428c
jcao03 <92761025+jcao03@users.noreply.github.com>
2021-10-22 11:29:10 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var a;
 function addName() {
 //get value from input field and save as "a"
-    var a = document.getElementById("name").value;
+    var a = document.getElementById("name").value = "";
     window.alert(a)
 //create localStorage key with value of a
     localStorage.setItem("uName", a);
