eab6bdb93781d7107a300b4a21f7c31e74b9a3a9
jcao03 <92761025+jcao03@users.noreply.github.com>
2021-10-22 11:19:05 -1000

    Added localStorage and linked to page two.html

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 var a;
 function addName() {
 //get value from input field and save as "a"
-    var b = document.getElementById("name").value;
-    window.alert(b)
+    var a = document.getElementById("name").value;
+    window.alert(a)
 //create localStorage key with value of a
     localStorage.setItem("uName", a);
 //automatically take user to page two.html
