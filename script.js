ade96fdab3de6c9c3a9f1bc909e033a8b42e7867
jcao03 <92761025+jcao03@users.noreply.github.com>
2021-10-22 11:30:19 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var a;
 function addName() {
 //get value from input field and save as "a"
-    var a = document.getElementById("name").value = "";
-    window.alert(a)
+    var a = document.getElementById("name").value;
+    document.getElementById("name").value = "";
 //create localStorage key with value of a
     localStorage.setItem("uName", a);
 //automatically take user to page two.html
